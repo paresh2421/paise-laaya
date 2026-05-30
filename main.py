2c9b65414c4b7ea906b1d3a826f5cb5720b3fe0f
paresh2421 <pareshnair2411@gmail.com>
2026-05-30 17:34:07 +0530

    adds the frontend and other data

--- main.py
+++ main.py
@@ -1,7 +1,54 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from contextlib import asynccontextmanager
+from database import create_db_and_tables, engine
+from models import Category, Account
+from sqlmodel import Session, select
+from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Connecting to the Database...")
+    create_db_and_tables()
+    yield
+    print("Safely shutting down...")
+
+app = FastAPI(lifespan=lifespan)
+
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-def read_root():
-    return {"message":"hello world"}
+def read_root(request: Request):
+    with Session(engine) as session:
+        accounts = session.exec(select(Account)).all()
+        categories = session.exec(select(Category)).all()
+        return templates.TemplateResponse(
+            request=request, name="dashboard.html",context={"accounts":accounts, "categories":categories} 
+        )
+
+@app.post("/accounts/")
+def create_account(account: Account):
+    with Session(engine) as session:
+        session.add(account)
+        session.commit()
+        session.refresh(account)
+        return account
+
+@app.post("/categories/")
+def create_category(category: Category):
+    with Session(engine) as session:
+        session.add(category)
+        session.commit()
+        session.refresh(category)
+        return category
+    
+@app.get("/accounts/")
+def get_accounts():
+    with Session(engine) as session:
+        accounts = session.exec(select(Account)).all()
+        return accounts
+
+@app.get("/categories/")
+def get_categories():
+    with Session(engine) as session:
+        categories = session.exec(select(Category)).all()
+        return categories
--- models.py
+++ models.py
@@ -1,4 +1,22 @@
 from sqlmodel import SQLModel, Field
 from datetime import date
 
-# class Expense(SQLModel, table=True):
+class Category(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(unique=True)
+
+class Account(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(unique=True) #bank name
+    balance: float = Field(default=0.0)
+
+class Transaction(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    amount: float
+    type: str
+    note: str
+    transaction_date: date = Field(default_factory=date.today)
+
+    #foreign keys to account and category table
+    account_id: int | None = Field(default=None, foreign_key="account.id")
+    category_id: int | None = Field(default=None, foreign_key="category.id")
