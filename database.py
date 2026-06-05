a1213d21484e346eadfed8d572737c550b56514c
paresh2421 <pareshnair2411@gmail.com>
2026-06-05 15:10:14 +0530

    some new additions, still under development

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 from sqlmodel import SQLModel, create_engine
 import os
+from dotenv import load_dotenv
 
-# Paste your exact Neon URL here!
-DATABASE_URL = "postgresql://neondb_owner:npg_EPDu9mrITeo5@ep-proud-silence-aoklkqh4.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+load_dotenv()
 
-# Notice we removed connect_args={"check_same_thread": False} because Postgres doesn't need it!
+DATABASE_URL = os.getenv("DB_URL")
+
 engine = create_engine(DATABASE_URL, echo=True)
 
 def create_db_and_tables():
